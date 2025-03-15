5329bd20546a4ec9d04d71ba79821a45c7754248
Vasile-Marinel <vasilemarinel2136@gmail.com>
2025-03-15 19:04:58 +0200

    Design Page resize shape(front)

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -177,7 +177,7 @@ const Main = () => {
                                 </div>
                                 <div onClick={()=>createShape('shape','circle')} className='h-[90px] bg-[#3c3c3d] cursor-pointer rounded-full'>
                                 </div>
-                                <div onClick={()=>createShape('shape','trangle')} style={{clipPath: 'polygon(50% 0, 100% 100%, 0 100%)'}} className='h-[90px] bg-[#3c3c3d] cursor-pointer'></div>
+                                <div onClick={()=>createShape('shape','triangle')} style={{clipPath: 'polygon(50% 0, 100% 100%, 0 100%)'}} className='h-[90px] bg-[#3c3c3d] cursor-pointer'></div>
                             </div>
                         }
 
